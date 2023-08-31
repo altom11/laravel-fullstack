359c6a218002d253713834f966a8d26a3118660c
altom11 <aldian.umbu@gmail.com>
2023-08-31 16:30:53 +0700

    upload image api

---
+++ app/Http/Controllers/Api/UploadController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UploadController extends Controller
+{
+
+    public function uploadImage(Request $request)
+    {
+        if ($request->has('image')) {
+            $image = $request->image;
+            $nameFile = time() . '.' . $image->getClientOriginalExtension();
+            $path = public_path('upload/images');
+            $image->move($path, $nameFile);
+
+            return response()->json([
+                'image_path' => '/upload/images/' . $nameFile,
+                'base_url' => url('/'),
+            ]);
+        }
+    }
+
+    public function uploadMultipleImage(Request $request)
+    {
+        if ($request->has('image')) {
+            $images = $request->image;
+            foreach ($images as $key => $image) {
+                $nameFile = time() . $key . '.' . $image->getClientOriginalExtension();
+                $path = public_path('upload/images');
+                $image->move($path, $nameFile);
+            }
+            return response()->json([
+                'status' => 'upload successfully',
+            ]);
+        }
+    }
+}
